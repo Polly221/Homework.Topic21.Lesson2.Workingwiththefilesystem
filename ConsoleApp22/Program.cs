ff6745184b90612a8b62576d7d19b23048fa3bd8
Polinka <Polinka@DESKTOP-MHBJDTO>
2023-07-06 13:34:40 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+Console.Write("Введите путь к  файлу: ");
+string filePath = Console.ReadLine()! + ".txt";
+FileInfo fileInfo = new FileInfo(filePath);
+
+if (File.Exists(filePath))
+    {
+    Console.WriteLine("Файл существует.");
+    Console.WriteLine("Размер: " + fileInfo.Length + " байт");
+    Console.WriteLine("Последнее изм.: " + fileInfo.LastWriteTime);
+    Console.WriteLine("Расширение: " + fileInfo.Extension);
+}
+else
+    {
+    Console.WriteLine("Файл не найден.");
+    }
